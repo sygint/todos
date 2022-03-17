7c43d994d2c0c5e889187fac97439246aa9a5e19
Syg <sygint@users.noreply.github.com>
2022-03-17 08:10:55 -0700

    Add husky and lint-staged

---
+++ lint-staged.js
@@ -0,0 +1,8 @@
+module.exports = {
+    '*.{js,jsx,ts,tsx}': [
+        'eslint --max-warnings=0', 
+        'react-scripts test --bail --watchAll=false --findRelatedTests --passWithNoTests',
+        () => 'tsc-files --noEmit',
+    ],
+    '*.{json,css}': ['prettier --write'],
+}
