354e5314d6ae041eb95c92b4d14c5df6483c18d7
Syg <sygint@users.noreply.github.com>
2022-03-20 18:31:09 -0700

    Format and add --fix to eslint command in lint-staged

--- lint-staged.js
+++ lint-staged.js
@@ -1,8 +1,8 @@
 module.exports = {
-    '*.{js,jsx,ts,tsx}': [
-        'eslint --max-warnings=0', 
-        'react-scripts test --bail --watchAll=false --findRelatedTests --passWithNoTests',
-        () => 'tsc-files --noEmit',
-    ],
-    '*.{json,css}': ['prettier --write'],
-}
+  "*.{js,jsx,ts,tsx}": [
+    "eslint --max-warnings=0 --fix",
+    "react-scripts test --bail --watchAll=false --findRelatedTests --passWithNoTests",
+    () => "tsc-files --noEmit",
+  ],
+  "*.{json,css}": ["prettier --write"],
+};
