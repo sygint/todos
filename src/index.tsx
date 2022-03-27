1e1dbcbaca7e16dce40618b933ba27b37c98fa50
syg <sygint@users.noreply.github.com>
2022-03-27 08:12:50 -0700

    Move global styles out of scss

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./components/App";
+import { GlobalStyle } from "./components/styles";
 import reportWebVitals from "./reportWebVitals";
-
-import "./index.scss";
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById("root"),
